0176860a92bdad1ae8ad08d56f262a219df6fa7f
Wesley-Costa <wesleycosta06@hotmail.com>
2026-03-19 17:54:05 -0300

    feat: criação e listagem das tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,53 +2,67 @@ import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Meteor } from "meteor/meteor";
 import { useTracker } from "meteor/react-meteor-data";
+import { Box, CircularProgress } from "@mui/material";
 
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { ForgotPasswordPage } from "./pages/ForgotPasswordPage";
 import { ResetPasswordPage } from "./pages/ResetPasswordPage";
-
-function HomePage() {
-  return (
-    <div>
-      <h1>Bem-vindo! Usuário logado.</h1>
-      <button onClick={() => Meteor.logout()}>Logout</button>
-    </div>
-  );
-}
+import { ListTasksPage } from "./pages/ListTasksPage";
+import { RegisterTaskPage } from "./pages/RegisterTaskPage";
+import "./styles/styles.css";
 
 export const App = () => {
-  const user = useTracker(() => Meteor.user());
+  const user = useTracker(() => {
+    return Meteor.user();
+  });
 
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path="/login"
-          element={user ? <Navigate to="/" replace /> : <LoginPage />}
+          element={user ? <Navigate to="/tasks" replace /> : <LoginPage />}
         />
-
         <Route
           path="/register"
-          element={user ? <Navigate to="/" replace /> : <RegisterPage />}
+          element={user ? <Navigate to="/tasks" replace /> : <RegisterPage />}
         />
-
         <Route
           path="/forgot-password"
-          element={user ? <Navigate to="/" replace /> : <ForgotPasswordPage />}
+          element={
+            user ? <Navigate to="/tasks" replace /> : <ForgotPasswordPage />
+          }
         />
-
         <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-
         <Route
           path="/"
           element={
-            <ProtectedRoute>
-              <HomePage />
+            user ? (
+              <Navigate to="/tasks" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+        <Route
+          path="/tasks"
+          element={
+            <ProtectedRoute user={user}>
+              <ListTasksPage />
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/tasks/register"
+          element={
+            <ProtectedRoute user={user}>
+              <RegisterTaskPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
